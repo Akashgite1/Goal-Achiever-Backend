528b8301cf1a59607d6e852ae847ea8918a65a75
Akashgite1 <akashgite550@gmail.com>
2025-09-14 02:29:48 +0530

    Remove ignored files from repo

--- testDB.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
-const uri = process.env.MONGO_URI;
-
-mongoose.connect(uri)
-  .then(() => console.log("DB connected"))
-  .catch(err => console.error("DB connection failed:", err));
